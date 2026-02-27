a2327ed27cb92d9bf986823471e6bd0cb68d7ed0
Ayobami <oludemiayobami@gmail.com>
2026-02-27 09:34:40 +0100

    refactor(assets): update asset structure

--- src/common/svgs.ts
+++ src/common/svgs.ts
@@ -1,27 +1,1 @@
-import CartSVG from '../../assets/images/cart.svg';
-import PackageSVG from '../../assets/images/package.svg';
-import RegisterSVG from '../../assets/images/register.svg';
-import TruckSVG from '../../assets/images/truck.svg';
-import QuestionMarkSVG from '../../assets/images/questionMark.svg';
-import BackIconSVG from '../../assets/images/back.svg';
-import LocationSVG from '../../assets/images/location.svg';
-import NotificaionSVG from '../../assets/images/notification.svg';
-import HomeSVG from '../../assets/images/home.svg';
-import SaveSVG from '../../assets/images/save.svg';
-import BagSVG from '../../assets/images/bag.svg';
-import UserSVG from '../../assets/images/user.svg';
-
-export {
-  CartSVG,
-  PackageSVG,
-  RegisterSVG,
-  TruckSVG,
-  QuestionMarkSVG,
-  BackIconSVG,
-  LocationSVG,
-  NotificaionSVG,
-  HomeSVG,
-  UserSVG,
-  BagSVG,
-  SaveSVG,
-};
+import CartSVG from '@assets/images/cart.svg';import PackageSVG from '@assets/images/package.svg';import RegisterSVG from '@assets/images/register.svg';import TruckSVG from '@assets/images/truck.svg';import QuestionMarkSVG from '@assets/images/questionMark.svg';import BackIconSVG from '@assets/images/back.svg';import LocationSVG from '@assets/images/location.svg';import NotificationSVG from '@assets/images/notification.svg';import HomeSVG from '@assets/images/home.svg';import SaveSVG from '@assets/images/save.svg';import BagSVG from '@assets/images/bag.svg';import UserSVG from '@assets/images/user.svg';export {  CartSVG,  PackageSVG,  RegisterSVG,  TruckSVG,  QuestionMarkSVG,  BackIconSVG,  LocationSVG,  NotificationSVG,  HomeSVG,  UserSVG,  BagSVG,  SaveSVG,};
